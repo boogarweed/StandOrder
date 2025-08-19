050a472c0e04ed9542c44fbe4e0de8cfb668e52f
Kelly Snodgrass <kelly.snodgrass@gmail.com>
2025-08-19 14:15:49 -0500

    Added audible error messages to pages and fixed list ordering on Returns and Walk Throughs.

---
+++ StandOrder/wwwroot/js/audioPlayer.js
@@ -0,0 +1,13 @@
+﻿window.audioPlayer = {
+    play: (soundFile) => {
+        // Create a new Audio object
+        // The soundFile path will be relative to the wwwroot folder, e.g., 'sounds/error.mp3'
+        var audio = new Audio(soundFile);
+
+        // Set volume to maximum
+        audio.volume = 1.0;
+
+        // Play the sound
+        audio.play().catch(e => console.error("Error playing sound:", e));
+    }
+};
